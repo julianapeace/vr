502ffa195f6e08e0fc46a3402f496a8e0975c156
julianapeace <chancecordelia@gmail.com>
2019-05-14 14:50:38 -0400

    sphere

--- client.js
+++ client.js
@@ -1,26 +1,71 @@
-// This file contains the boilerplate to execute your React app.
-// If you want to modify your application's content, start in "index.js"
 
-import {ReactInstance} from 'react-360-web';
+import { ReactInstance, Surface } from 'react-360-web';
 
 function init(bundle, parent, options = {}) {
+
+  const SIZE = 300;
+
   const r360 = new ReactInstance(bundle, parent, {
-    // Add custom options here
     fullScreen: true,
     ...options,
   });
 
-  // Render your app content to the default cylinder surface
+  // SPHERE
+  const sphereSurface = new Surface(
+    SIZE,
+    SIZE,
+    Surface.SurfaceShape.Flat,
+  );
+  sphereSurface.setAngle(0, 0);
   r360.renderToSurface(
-    r360.createRoot('Hello360', { /* initial props */ }),
-    r360.getDefaultSurface()
+    r360.createRoot('Info', { name: 'Sphere' }),
+    sphereSurface,
   );
 
-  // Load the initial environment
-  r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
+  // CAPSULE
+  const capsuleSurface = new Surface(
+    SIZE,
+    SIZE,
+    Surface.SurfaceShape.Flat,
+  );
+  capsuleSurface.setAngle(Math.PI / 2, 0);
+  r360.renderToSurface(
+    r360.createRoot('Info', { name: 'Capsule' }),
+    capsuleSurface,
+  );
 
-  // To play a video as your background, reference it by its unique id
-  // r360.compositor.setBackgroundVideo('myplayer');
+  // CYLINDER
+  const cylinderSurface = new Surface(
+    SIZE,
+    SIZE,
+    Surface.SurfaceShape.Flat,
+  );
+  cylinderSurface.setAngle(Math.PI, 0);
+  r360.renderToSurface(
+    r360.createRoot('Info', { name: 'Cylinder' }),
+    cylinderSurface,
+  );
+
+  // CUBE
+  const cubeSurface = new Surface(
+    SIZE,
+    SIZE,
+    Surface.SurfaceShape.Flat,
+  );
+  cubeSurface.setAngle(-1 * Math.PI / 2, 0);
+  r360.renderToSurface(
+    r360.createRoot('Info', { name: 'Cube' }),
+    cubeSurface,
+  );
+
+  // 3D SPHERE
+  r360.renderToLocation(
+    r360.createRoot('Sphere'),
+    r360.getDefaultLocation(),
+  );
+
+  r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
+  // r360.compositor.setBackground(r360.getAssetURL('3d_scene.jpg'));
 }
 
 window.React360 = {init};
--- index.js
+++ index.js
@@ -1,61 +1,6 @@
-import React from 'react';
-import {
-  Environment,
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  VrButton,
-} from 'react-360';
+import { AppRegistry } from 'react-360';
+import Info from './src/components/Info';
+import Sphere from './src/components/Sphere';
 
-export default class Hello360 extends React.Component {
-  state = {
-    count: 0,
-  };
-
-  // This method increments our count, triggering a re-render
-  _incrementCount = () => {
-    this.setState({count: this.state.count + 1});
-  };
-
-  // Once the component mounts, run the increment method every second
-  componentDidMount() {
-    // setInterval(this._incrementCount, 1000);
-  }
-
-  render() {
-    return (
-      <View style={styles.panel}>
-      <VrButton
-        onClick={this._incrementCount}
-        style={styles.greetingBox}>
-        <Text style={styles.greeting}>
-          {`Count: ${this.state.count}`}
-        </Text>
-      </VrButton>
-    </View>
-    );
-  }
-};
-
-const styles = StyleSheet.create({
-  panel: {
-    // Fill the entire surface
-    width: 1000,
-    height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  greetingBox: {
-    padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
-    borderWidth: 2,
-  },
-  greeting: {
-    fontSize: 30,
-  },
-});
-
-AppRegistry.registerComponent('Hello360', () => Hello360);
+AppRegistry.registerComponent('Info', () => Info);
+AppRegistry.registerComponent('Sphere', () => Sphere);
